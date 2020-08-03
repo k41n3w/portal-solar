f1eecac659708e187cb1e743eb730699f95e437c
Caio Ramos <kaineo@hotmail.com>
2020-08-02 23:31:07 -0300

    Add PowerGenerator methods automatized tests

---
+++ spec/models/power_generator_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+require 'factories/users'
+require 'factories/power_generators'
+
+RSpec.describe PowerGenerator, type: :model do
+	context 'find_principal_answers(current_user)' do
+		let!(:user) { create(:user) }
+		let!(:power_generator) { create(:power_generator) }
+		
+		it 'expect some power_generators' do
+			expect(PowerGenerator.find_principal_answers(user)).not_to be_nil
+		end
+	end
+
+	context 'find_max_value(answer, attribute, power_generators)' do
+		let!(:user) { create(:user, question_6: '9.0') }
+		let!(:power_generator_1) { create(:power_generator, kwp: '100.0') }
+		let!(:power_generator_2) { create(:power_generator, kwp: '7.0') }
+		
+		it 'expect some power_generators' do
+			power_generators = []
+
+			power_generators = PowerGenerator.all
+
+			expect(PowerGenerator.find_max_value(user.question_6, 'kwp', power_generators).count).to eq(1)
+		end
+	end
+
+	context 'find_structure_type(answer, perfect_match)' do
+		let!(:user) { create(:user, question_7: 'metalico') }
+		let!(:power_generator_1) { create(:power_generator, structure_type: 'metalico') }
+		let!(:power_generator_2) { create(:power_generator, structure_type: 'laje') }
+		
+		it 'expect some power_generators' do
+			power_generators = []
+
+			perfect_match = PowerGenerator.all
+
+			expect(PowerGenerator.find_structure_type(user.question_7, perfect_match).count).to eq(1)
+		end
+	end
+end
