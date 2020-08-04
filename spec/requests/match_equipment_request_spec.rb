345c433aac1ade78d691045087e300846bbc1a9f
Caio Ramos <kaineo@hotmail.com>
2020-08-04 18:28:28 -0300

    Add test for MatchEquipmentController none item case

--- spec/requests/match_equipment_request_spec.rb
+++ spec/requests/match_equipment_request_spec.rb
@@ -1,27 +1,39 @@
 require 'rails_helper'
+require 'factories/users'
+require 'factories/power_generators'
 
 RSpec.describe "MatchEquipments", type: :request do
-
   describe 'root GET /' do
-    let!(:user) { create(:user) }
-    let!(:power_generator_1) { create(:power_generator) }
-    let!(:power_generator_2) { create(:power_generator) }
+    context 'with a perfect match on list' do
+      let!(:user) { create(:user) }
+      let!(:power_generator_1) { create(:power_generator) }
+      let!(:power_generator_2) { create(:power_generator) }
 
+      before do
+        sign_in user
+      end
 
-    before do
-      sign_in user
+      subject do
+        response
+      end
+      
+      before do
+        get(match_equipment_index_path)
+      end
+      
+      it 'must respond with status code :ok (200)' do
+        is_expected.to have_http_status(:ok)
+      end
     end
 
-    subject do
-      response
+    context 'without a perfect match on list' do
+      it { expect(@perfect_matcht).to be_nil }
     end
-    
-    before do
-      get(match_equipment_index_path)
-    end
-    
-    it 'must respond with status code :ok (200)' do
-      is_expected.to have_http_status(:ok)
+
+    context 'with none item on list' do
+      it { expect(@power_generators).not_to be }
+      
+      it { expect(@perfect_matcht).not_to be }
     end
   end
 end
