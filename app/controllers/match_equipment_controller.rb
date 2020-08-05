84f7cc2dd6a2711f0cfe024cd7a91219d9e7f0d7
Caio Ramos <kaineo@hotmail.com>
2020-08-04 23:53:40 -0300

    Refactor for simplecov. Now we've 99.31% covered

--- app/controllers/match_equipment_controller.rb
+++ app/controllers/match_equipment_controller.rb
@@ -5,8 +5,8 @@ class MatchEquipmentController < ApplicationController
     return redirect_to new_user_registration_path if current_user.nil?
 
     @power_generators = PowerGenerator.find_principal_generators(current_user)
-    @perfect_match = PowerGenerator.find_perfect_match(current_user, @power_generators)
 
+    @perfect_match = PowerGenerator.find_perfect_match(current_user, @power_generators)
     @q = @power_generators.ransack(params[:q])
     @q = @perfect_match.ransack(params[:q])
   end
--- spec/requests/match_equipment_request_spec.rb
+++ spec/requests/match_equipment_request_spec.rb
@@ -7,8 +7,26 @@ require 'factories/power_generators'
 RSpec.describe 'MatchEquipments', type: :request do
   describe 'root GET /' do
     context 'with a perfect match on list' do
-      let!(:user) { create(:user) }
-      let!(:power_generator_1) { create(:power_generator) }
+      let!(:user) { create(:user,
+                            question_1: 'Destinado principalmente a uso comercial, empresarial.',
+                            question_2: 'Garantia simples.',
+                            question_3: 'Display simples.',
+                            question_4: '10.000,00',
+                            question_5: 100,
+                            question_6: 5,
+                            question_7: %w[laje].first
+                          ) 
+                  }
+
+      let!(:power_generator_1) { create(:power_generator,
+                                 description: 'Destinado principalmente a uso comercial, empresarial.
+                                               Garantia simples.
+                                               Display simples.',
+                                  price: '10.000,00',
+                                  weight: 99,
+                                  kwp: 4,
+                                  structure_type: %w[laje].first) }
+
       let!(:power_generator_2) { create(:power_generator) }
 
       before do
@@ -23,19 +41,57 @@ RSpec.describe 'MatchEquipments', type: :request do
         get(match_equipment_index_path)
       end
 
-      it 'must respond with status code :ok (200)' do
-        is_expected.to have_http_status(:ok)
+      it 'should bring the perfect match' do
+        expect(response.body).to include("Encotramos exatamente o que você procura:")
       end
+
+      it { is_expected.to have_http_status(:ok) }
     end
 
     context 'without a perfect match on list' do
-      it { expect(@perfect_matcht).to be_nil }
+      let!(:user) { create(:user,
+                            question_1: 'A',
+                            question_2: 'B',
+                            question_3: 'C',
+                            question_4: 'D',
+                            question_5: 999,
+                            question_6: 99,
+                            question_7: %w[metalico].first
+                          ) 
+                  }
+
+      let!(:power_generator_1) { create(:power_generator,
+                                  description: 'Destinado principalmente a uso comercial, empresarial.
+                                                Garantia simples.
+                                                Display simples.',
+                                  price: '10.000,00',
+                                  weight: 99,
+                                  kwp: 4,
+                                  structure_type: %w[laje].first) }
+
+      before do
+        sign_in user
+      end
+
+      subject do
+        response
+      end
+
+      before do
+        get(match_equipment_index_path)
+      end
+
+      it 'should bring the perfect match' do
+        expect(response.body).not_to include("Encotramos exatamente o que você procura:")
+      end
+
+      it { is_expected.to have_http_status(:ok) }
     end
 
     context 'with none item on list' do
       it { expect(@power_generators).not_to be }
 
-      it { expect(@perfect_matcht).not_to be }
+      it { expect(@perfect_match).not_to be }
     end
   end
 end
